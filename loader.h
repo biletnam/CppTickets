4a744a0b7a23a1a10068998a35c220ece4433e79
m1nsk <aleksey.korolev@biovitrum.ru>
2015-09-28 03:17:29 +0300

    Update loader.h

--- loader.h
+++ loader.h
@@ -10,6 +10,7 @@ using namespace std;
 vector<string*> divideTicketString(string str,char symbol); //divide string with symbol and returns vector of strings
 string* copyStringPointToPoint(char* s,char* e);            //returns copy of part of string between two pointers      
 char* findSymbolFromPoint(char* start,char symbol);         //finds next symbol in string and return pointer to this symbol
+pPair findParamInParamPairs(string* str,char* params[],int qty);
 void test();
 
 #endif // LOAD_H_INCLUDED
